e736f51b667f1468c7b5953e7b7ffec01c9bfad1
Alexandre Machado <alexandre.machcruz@gmail.com>
2024-04-18 17:48:22 -0300

    Add policy details

--- app.rb
+++ app.rb
@@ -39,4 +39,33 @@ class App < Sinatra::Base
 
     erb :policies, locals: { policies: JSON.parse(response.body)['data']['policies'] }
   end
+
+  get '/policies/:id' do
+    query = <<-GRAPHQL
+      query GetPolicy($id: ID!) {
+        policy(id: $id) {
+          effectiveFrom
+          effectiveUntil
+          insured {
+            name
+            documentNumber
+          }
+          vehicle {
+            licensePlate
+            make
+            model
+            year
+          }
+        }
+      }
+    GRAPHQL
+
+    client = Faraday.new(url: ENV['GRAPHQL_HOST'], headers: { 'Content-Type' => 'application/json' })
+
+    policy = client.post do |req|
+      req.body = { query: query, variables: { id: params[:id] } }.to_json
+    end
+
+    erb :policy_details, locals: { policy: JSON.parse(policy.body)['data']['policy'] }
+  end
 end
